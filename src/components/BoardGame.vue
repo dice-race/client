01f68edd30f8f48cf7159f272c29de45ae1143f5
khodhirob <odirobbani@gmail.com>
2018-08-23 23:42:01 +0700

    player1 testing with board

--- src/components/BoardGame.vue
+++ src/components/BoardGame.vue
@@ -1,12 +1,13 @@
 <template>
     
         <div id="example">
-        <div class="row board">
+        <div class="row bg-dark board">
             <div id="player1" v-bind:style="{ top: topPosition1 ,left: leftPosition1 }"></div>
             
 
         </div>
 
+      
         <div>
             <button v-on:click="changePosition1">Change</button>
            
@@ -18,8 +19,8 @@
 export default {
   data() {
     return {
-      position1x: "25px",
-      position1y: "75px",
+      position1x: "50px",
+      position1y: "500px",
       movement1: 0
     };
   },
@@ -37,37 +38,39 @@ export default {
       this.movement1 += move1;
 
       if (this.movement1 <= 5) {
-        let x = this.movement1 * 50;
+        let x = this.movement1 *  50+ (50*(this.movement1-1));
         this.position1x = `${x}px`;
         this.position1y = "500px";
       } 
       else if (this.movement1 <= 10) {
-        let x = this.movement1 * 50 - (50 + (this.movement1 - 6) * 100);
+        // alert(this.movement1*50)
+        let x = this.movement1 * 50 + 150 - ((this.movement1 - 6) * 150);
         this.position1x = `${x}px`;
-        this.position1y = "400px";
+        this.position1y = "400px";    
       }
        else if (this.movement1 <= 15) {
-        let x = this.movement1 * 50 - 500;
+        let x = this.movement1 * 50 - 500 + (50 * (this.movement1 - 11));
         this.position1x = `${x}px`;
         this.position1y = "300px";
       }
        else if (this.movement1 <= 20) {
-        let x = this.movement1 * 50 - (550 + (this.movement1 - 16) * 100);
+        let x = this.movement1 * 50 - (350 + ((this.movement1 - 16) * 150));
         this.position1x = `${x}px`;
         this.position1y = "200px";
       }
        else if (this.movement1 < 25) {
-        let x = this.movement1 * 50 - 1000;
+        let x = this.movement1 * 50 - (1000-(50*(this.movement1-21)));
       
         this.position1x = `${x}px`;
         this.position1y = "100px";
+
       }
 
       else if(this.movement1 >= 25){
-         this.position1x = `250px`;
+         this.position1x = `450px`;
         this.position1y = "100px";        
         
-        alert('finish')
+        
       }
       
      
@@ -79,10 +82,11 @@ export default {
 
 <style>
 #player1 {
-  background-color: green;
-  height: 100px;
-  width: 50px;
+  background-image: url('kim-jong-il.png');
+  height: 60px;
+  width: 60px;
   position: absolute;
+  margin-top: 20px;
 }
 
 #player2 {
@@ -92,7 +96,11 @@ export default {
   position: absolute;
 }
 
-/* .board{
-            background-image: url('dice-board.png')
-        } */
+.board{
+  background-image: url('dice-board.png');
+ height: 500px;
+  width: 500px;
+  margin-left: 42px;
+  margin-top: 14.5px;
+ }
 </style>
