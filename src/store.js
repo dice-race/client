747911541c041e5f2a546d4657bd850c398fb2f2
rizariza69 <riza.riza69@gmail.com>
2018-08-24 10:23:41 +0700

    login page

--- src/store.js
+++ src/store.js
@@ -1,16 +1,33 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import db from '@/firebaserc.js'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    username: ''
   },
   mutations: {
-
+    setName(state, payload) {
+      state.username = payload
+    }
   },
   actions: {
+    createUser() {
+      console.log(this.state.username)
+      // console.log('gggggg');
+      
+      db.ref('users/').push({
+        username: this.state.username,
+      }, function (error) {
+        if (error) {
+          console.log('error')
+        } else {
+          console.log('Data saved successfully')
+        }
+      });
+    }
 
   }
 })
---
+++ src/views/login.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+   <login></login>
+
+</div>
+
+</template>
+
+<script>
+import Login from '@/components/ComLogin.vue'
+
+export default {
+  components: {
+    Login
+  }
+}
+</script>
+
+
